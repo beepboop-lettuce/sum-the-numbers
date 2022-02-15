ab87e97ef00dffdacc4f77522aaa89a871ab3bd2
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-15 11:27:22 -0600

    defined size var for loop to iterate size inner loop to iterate size adding outer and inner vals

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	size := 10
+
+	for i := 1; i <= size; i++ {
+		for j := 1; j <= size; j++ {
+			fmt.Printf("Sum: %d\n", i+j)
+			fmt.Println()
+		}
+	}
 }
