9e9db000cc500a3fb2105d45db39c1dbae553e58
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-15 13:18:17 -0600

    experimenting with two ways to sum #s in a slice using for loops i incrementing range

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	size := 10
+	size := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	for i := 1; i <= size; i++ {
-		for j := 1; j <= size; j++ {
-			fmt.Printf("Sum: %d\n", i+j)
-			fmt.Println()
-		}
+	cumSum := 0
+
+	//only one or the other of these for loops
+	//these are just to see two different ways it can be done
+
+	for i := 1; i <= len(size); i += 2 {
+		cumSum += size[i] + size[i-1]
 	}
+	fmt.Printf("Sum: %d\n", cumSum)
+
+	for _, value := range size {
+		cumSum += value
+	}
+	fmt.Printf("Sum: %d\n", cumSum)
 }
